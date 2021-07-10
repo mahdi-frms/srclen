e462f4b9539ca115d9f1d22a2b393017280f53f7
mahdi-frms <mahdif1380@outlook.com>
2021-07-10 11:00:07 +0430

    basic line counting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[allow(dead_code)]
+fn count(text:&str)->usize{
+    let mut counter = 0;
+    for c in text.chars() {
+        if c == '\n'{
+            counter += 1;
+        }
+    }
+    counter + 1
+}
+
+#[cfg(test)]
+mod test {
+    use crate::count;
+
+    #[test]
+    fn counts_lines(){
+        let text = 
+"fn main() {
+    println!(\"Hello, world!\");
+}";
+        assert_eq!(count(text),3);
+    }
+}
